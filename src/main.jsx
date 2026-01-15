ad54830bfb9c02613047f9b21459579b15fc3500
SANTHOSHG-WEB <santhoshg22333@gmail.com>
2026-01-15 10:54:28 +0530

    Hardcode credentials for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 import App from './App.jsx'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const clientId = "1059101358909-gv04foq077mnmecu68aqtl96sj2rvo7u.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
