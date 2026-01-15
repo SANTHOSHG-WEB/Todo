90a4811cb41e8a2443dae3fb47aea6d3a0199281
SANTHOSHG-WEB <santhoshg22333@gmail.com>
2026-01-15 15:49:42 +0530

    Database: Finalized with the correct Supabase public key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://lnprltvscraxrvguxhua.supabase.co'
-const supabaseKey = 'sb_secret_LoOQ2JEl-I6Nlb1DToi9Gg_sojrChke'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxucHJsdHZzY3JheHJ2Z3V4aHVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NjAwNDYsImV4cCI6MjA4NDAzNjA0Nn0.XxqxeMXoncdBxFvTkyYLjekFJaIIYpZfQwKAfh5bAnY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
