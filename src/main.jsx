22bf849d3b15bf5f5213d692d42687f64908b5b1
SANTHOSHG-WEB <santhoshg22333@gmail.com>
2026-01-15 15:25:19 +0530

    Cleanup: Remove unused Google OAuth code and successfully migrate to Supabase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 import App from './App.jsx'
-
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "1059101358909-gv04foq077mnmecu68aqtl96sj2rvo7u.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
